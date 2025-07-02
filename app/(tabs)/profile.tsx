aac519acdfb1c6f8c325531cef3f290acf2922d6
Yixuan Guo <easonguo100@outlook.com>
2025-07-02 16:44:43 +0930

    feat: change the profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const profile = () => {
   return (
-    <View>
-      <Text>profile</Text>
-    </View>
+    <SafeAreaView className='flex-1 bg-myBackGround'>
+      <View>
+        <Text>{'Profile'}</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
