bb8fb3bda7e310f2c46436d7673ff138289c8f5e
Yixuan Guo <easonguo100@outlook.com>
2025-07-02 16:44:12 +0930

    feat: edited the content of home

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,56 +1,22 @@
 import React, { useState } from "react";
 import { Image } from "expo-image";
-import { Platform, StyleSheet } from "react-native";
-
+import { View, Text, Platform, StyleSheet } from "react-native";
 import { HelloWave } from "@/components/HelloWave";
 import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 import { Button } from "@react-navigation/elements";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-  const [showGreeting, setShowGreeting] = useState(false);
 
-  const handleClick = () => {
-    setShowGreeting(!showGreeting);
-  };
+
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <Button onPressIn={handleClick}>{showGreeting ? "Hide" : "Show"}</Button>
-      {showGreeting && (
-        <ThemedView style={styles.titleContainer}>
-          <ThemedText type='title'>Hello!</ThemedText>
-          <HelloWave />
-        </ThemedView>
-      )}
-    </ParallaxScrollView>
+    <SafeAreaView className='flex-1 bg-myBackGround'>
+      <View>
+        <Text>{"Home"}</Text>
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
-  },
-});
