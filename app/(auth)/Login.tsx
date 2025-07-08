89d52a53a81517d9a5fe224baf19e788d5904ed5
Yixuan Guo <easonguo100@outlook.com>
2025-07-08 15:43:04 +0930

    feat: update login and signUp page

--- app/(auth)/Login.tsx
+++ app/(auth)/Login.tsx
@@ -1,11 +1,52 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, TextInput, Pressable } from "react-native";
+import React, { useState } from "react";
+import { Link } from "expo-router";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    console.log("login");
+  };
+
   return (
-    <View>
-      <Text>login</Text>
-    </View>
+    <SafeAreaView className='flex-1 bg-myBackGround'>
+      <View className='flex-1 flex-col mx-2 items-center'>
+        <Text className='text-2xl font-bold text-myGreen text-center mt-20'>
+          Profile
+        </Text>
+        <TextInput
+          placeholder='Email or username'
+          value={email}
+          onChangeText={setEmail}
+          className='border border-gray-400 rounded-md p-2 mt-16 h-14 w-4/5'
+        />
+        <TextInput
+          placeholder='Password'
+          value={password}
+          onChangeText={setPassword}
+          className='border border-gray-400 rounded-md p-2 mt-3 h-14 w-4/5'
+        />
+        <Pressable
+          className=' bg-green-500 border-gray-200 rounded-md p-2 mt-10 h-14 w-4/5 justify-center'
+          onTouchEnd={handleLogin}
+        >
+          <Text className='text-white text-center font-bold text-xl'>
+            Login
+          </Text>
+        </Pressable>
+        <View className='flex-row justify-center p-4 w-full'>
+          <Text className='text-center font-semibold text-sm'>
+            Haven't got an account?
+          </Text>
+          <Link href='/SignUp' className='font-semibold text-sm text-green-500'>
+            {" "}
+            Register
+          </Link>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
--- app/(auth)/SignUp.tsx
+++ app/(auth)/SignUp.tsx
@@ -1,11 +1,55 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, Pressable, SafeAreaView, TextInput } from "react-native";
+import React, { useState } from "react";
+import { Link } from "expo-router";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+    console.log("email:", email, "username:", username, "password:", password);
+  };
+
   return (
-    <View>
-      <Text>SignUp</Text>
-    </View>
+    <SafeAreaView className='flex-1 bg-myBackGround'>
+      <View className='flex-1 flex-col mx-2 items-center'>
+        <Text className='text-2xl font-bold text-myGreen text-center mt-20'>
+          Sign Up
+        </Text>
+        <TextInput
+          placeholder='Email'
+          value={email}
+          onChangeText={setEmail}
+          className='border border-gray-400 rounded-md p-2 mt-10 h-14 w-4/5'
+        />
+        <TextInput
+          placeholder='Username'
+          value={username}
+          onChangeText={setUsername}
+          className='border border-gray-400 rounded-md p-2 mt-3 h-14 w-4/5'
+        />
+        <TextInput
+          placeholder='Password'
+          value={password}
+          onChangeText={setPassword}
+          className='border border-gray-400 rounded-md p-2 mt-3 h-14 w-4/5'
+        />
+        <Pressable
+          className=' bg-green-500 border-gray-200 rounded-md p-2 mt-10 h-14 w-4/5 justify-center'
+          onTouchEnd={handleSubmit}
+        >
+          <Text className='text-white text-center font-bold text-xl'>
+            Submit
+          </Text>
+        </Pressable>
+        <View className='flex-row justify-center p-4 w-full'>
+          <Text className='text-center font-semibold text-sm'>Already had an account?</Text>
+          <Link href='/Login' className='font-semibold text-sm text-green-500'> Login</Link>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
