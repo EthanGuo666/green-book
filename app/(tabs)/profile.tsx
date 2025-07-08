230e701bd7be6d712f06ced58439f41d7818267b
Yixuan Guo <easonguo100@outlook.com>
2025-07-08 11:56:09 +0930

    feat: put links on profile page

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,12 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context';
+import { View, Text, SafeAreaView } from "react-native";
+import React from "react";
+import { Link } from "expo-router";
 
 const profile = () => {
   return (
     <SafeAreaView className='flex-1 bg-myBackGround'>
-      <View>
-        <Text>{'Profile'}</Text>
+      <View className='flex-1 flex-col items-center'>
+        <View className='flex-row gap-2 items-center justify-center'>
+          <Text className='text-2xl font-bold'>{"Profile"}</Text>
+        </View>
+        <Link href='/Login' className='p-4 rounded-lg w-full'>
+          <Text className='text-center font-semibold text-lg'>登录</Text>
+        </Link>
+        <Link href='/SignUp' className='p-4 rounded-lg w-full'>
+          <Text className='text-center font-semibold text-lg'>注册</Text>
+        </Link>
+        <Link href='/Login' className='p-4 rounded-lg w-full'>
+          <Text className='text-center font-semibold text-lg'>退出登录</Text>
+        </Link>
       </View>
     </SafeAreaView>
   );
